d92c19281dd8b6cb6333c1332b0216c2a3a01172
CHtsp <champtsp@gmail.com>
2021-11-25 22:28:33 +0700

    add programme

--- webproject/webapp/views.py
+++ webproject/webapp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.shortcuts import redirect
 from django.http import HttpResponse
-from django.contrib.auth.models import User,auth
-from django.contrib import messages
+from django.contrib import auth
 config = {
     'apiKey': "AIzaSyDUFgVOBI1mGpoESnvcq6qIE8z2BzSijVE",
     'authDomain': "fir-app-68c4f.firebaseapp.com",
@@ -12,55 +10,33 @@ config = {
     'messagingSenderId': "239247887438",
     'appId': "1:239247887438:web:1ee3d672ee6f8cc466871f"
 }
+ 
 # Create your views here.
 def dashboard(request):
-    # if not request.user.is_authenticated :
-    #     return redirect('/loginForm')
-
-    context = {'dashboard_page' : 'active'}
-    return render(request, 'dashboard.html', context)
+    return render(request, 'dashboard.html')
 
 def courses(request):
-    context = {'courses_page' : 'active'}
-    return render(request, 'courses.html', context)
-
-def loginForm(request):
-    return render(request, 'login.html')
+    return render(request, 'courses.html')
 
 def login(request):
-    username=request.POST.get('username')
-    password=request.POST.get('password')
+    return render(request, 'login.html')
 
-    user=auth.authenticate(username=username,password=password)
-
-    if user is not None :
-        auth.login(request,user)
-        return redirect('/dashboard')
-    else :
-        messages.add_message(request, messages.INFO, 'ไม่พบข้อมูลผู้ใช้งาน')
-        return render(request, 'login.html')
-
-def logout(request):
-    auth.logout(request)
-    return redirect('/loginForm')
+def signup(request):
+    return render(request, 'signup.html')
 
 def error(request):
     return render(request, 'error.html')
-
 def programme(request):
     return render(request, 'programme.html')
 
 def download(request):
-    context = {'download_page' : 'active'}
-    return render(request, 'download.html', context)
+    return render(request, 'download.html')
 
 def links(request):
-    context = {'links_page' : 'active'}
-    return render(request, 'links.html', context)
+    return render(request, 'links.html')
 
 def comingsoon(request):
-    context = {'comingsoon_page' : 'active'}
-    return render(request, 'comingsoon.html', context)
+    return render(request, 'comingsoon.html')
 
 def register(request):
     return render(request, "register.html")
@@ -70,28 +46,3 @@ def craft(request):
 
 def busreq(request):
     return render(request, "busreq.html")
-
-def signupForm(request):
-    return render(request, 'signup.html')
-
-def signup(request):
-    username=request.POST.get('username')
-    password=request.POST.get('password')
-    cfpassword=request.POST.get('cfpassword')
-    if password == cfpassword :
-        if User.objects.filter(username=username).exists():
-           messages.add_message(request, messages.INFO, 'ชื่อผู้ใช้งานซ้ำ')
-           messages.info(request,'ชื่อผู้ใช้งานซ้ำ')
-           return redirect('/signupForm')
-        else : 
-            user=User.objects.create_user(
-                username=username,
-                password=password
-            )
-            user.save()
-            auth.login(request,user)
-            return redirect('/dashboard')
-    else :
-        messages.add_message(request, messages.INFO, 'password and confirm password ต้องตรงกัน')
-        # messages.info(request,'password and confirm password ต้องตรงกัน')
-        return redirect('/signupForm')
--- webproject/webproject/settings.py
+++ webproject/webproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gmqvzc^$l80v0hlg9)$$_72wl%--gj@y6yvowhdmwi()8#-602'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
