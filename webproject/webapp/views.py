aa3c95ffe67adc9682921fe294da7f4caaec58ca
hudchiiw <hudchiiw@gmail.com>
2021-11-25 19:08:15 +0700

    update - add feature login

--- webproject/webapp/views.py
+++ webproject/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
-from django.contrib import auth
+from django.contrib.auth.models import User,auth
 config = {
     'apiKey': "AIzaSyDUFgVOBI1mGpoESnvcq6qIE8z2BzSijVE",
     'authDomain': "fir-app-68c4f.firebaseapp.com",
@@ -10,33 +11,48 @@ config = {
     'messagingSenderId': "239247887438",
     'appId': "1:239247887438:web:1ee3d672ee6f8cc466871f"
 }
- 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    context = {'dashboard_page' : 'active'}
+    return render(request, 'dashboard.html', context)
 
 def courses(request):
-    return render(request, 'courses.html')
+    context = {'courses_page' : 'active'}
+    return render(request, 'courses.html', context)
+
+def loginForm(request):
+    return render(request, 'loginForm.html')
 
 def login(request):
-    return render(request, 'login.html')
+    username=request.POST.get('username')
+    password=request.POST.get('password')
 
-def signup(request):
-    return render(request, 'signup.html')
+    user=auth.authentication(username=username,password=password)
+
+    if user is not None :
+        auth.login(request,user)
+        return redirect('/dashboard')
+    else :
+        messages.info(request,'ไม่พบข้อมูลผู้ใช้งาน')
+    return render(request, 'login.html')
 
 def error(request):
     return render(request, 'error.html')
+
 def programme(request):
     return render(request, 'programme.html')
 
 def download(request):
-    return render(request, 'download.html')
+    context = {'download_page' : 'active'}
+    return render(request, 'download.html', context)
 
 def links(request):
-    return render(request, 'links.html')
+    context = {'links_page' : 'active'}
+    return render(request, 'links.html', context)
 
 def comingsoon(request):
-    return render(request, 'comingsoon.html')
+    context = {'comingsoon_page' : 'active'}
+    return render(request, 'comingsoon.html', context)
 
 def register(request):
     return render(request, "register.html")
@@ -46,3 +62,17 @@ def craft(request):
 
 def busreq(request):
     return render(request, "busreq.html")
+
+def signupForm(request):
+    return render(request, 'signup.html')
+
+def signup(request):
+    username=request.POST.get('username')
+    password=request.POST.get('password')
+    user=User.objects.create_user(
+        username=username,
+        password=password
+    )
+
+    user.save()
+    return redirect('/dashboard')
