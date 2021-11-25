f03573bf6db8ffc9d5d0bbaaecb9f411b13842fe
CHtsp <champtsp@gmail.com>
2021-11-25 21:41:27 +0700

    updatelink, code

--- webproject/webproject/settings.py
+++ webproject/webproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'webapp',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'P@ssw0rd',
         'HOST': 'localhost',
         'PORT': '3306'
     }
